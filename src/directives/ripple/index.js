069297e6b815d703571c43e1a2251f742c983bbe
Ahsath <ahsath@gmail.com>
2021-08-24 21:19:43 -0500

    refactor(rippleHookDef): setup ripple hooks
    on mounted will set a `dataset.rippleColorClass` with the value provided object passed if any
    
    add the ripple listener fn onto the `dir` binding argument
    
    on unmounted removes the listener fn added on the `dir` binding object property
    
    on updated, set the new value class onto the `el` `dataset.rippleColorClass` if any

--- src/directives/ripple/index.js
+++ src/directives/ripple/index.js
@@ -1,7 +1,15 @@
-import mouseRipple from "./mouseRipple"
+import ripple from "./ripple"
 
 export default {
-    mounted(el) {
-        el.addEventListener('mousedown', mouseRipple, { passive: true })
+    mounted(el, binding) {
+        el.dataset.rippleColorClass = binding.value?.class || 'vuei-bg-black'
+        binding.dir.listener = ripple()
+        el.addEventListener('mousedown', binding.dir.listener, { passive: true })
+    },
+    unmounted(el, binding) {
+        el.removeEventListener('mousedown', binding.dir.listener, { passive: true })
+    },
+    updated(el, binding) {
+        el.dataset.rippleColorClass = binding.value?.class || 'vuei-bg-black'
     }
 }
