4a3bf8ea4a5ee01011d7baa674d1bc334b709eb2
Ahsath <ahsath@gmail.com>
2021-08-24 21:01:18 -0500

    test(ripple.spec.js): add factory fn for rendering vue templates

--- src/directives/ripple/__tests__/ripple.spec.js
+++ src/directives/ripple/__tests__/ripple.spec.js
@@ -7,10 +7,12 @@ jest.mock('../animate.js')
 import { render, fireEvent, waitFor } from '@testing-library/vue'
 import ripple from '..'
 
+const renderFactory = (template) => render(template, { global: { directives: { ripple } } })
+
 describe('ripple', () => {
     it('shows an span element inside the button when the mousedown event is triggered on it', async () => {
         const template = { template: '<button v-ripple style="width: 100px; height: 100px" />' }
-        const { getByRole, getByTestId } = render(template, { global: { directives: { ripple } } })
+        const { getByRole, getByTestId } = renderFactory(template)
         const button = getByRole('button')
 
         await fireEvent.mouseDown(button)
